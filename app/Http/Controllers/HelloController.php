4a27f7d438b32ae7ae5cf4588b77c4b2e9517ffa
yoshioka <yussan5555@gmail.com>
2022-10-08 14:15:59 +0900

    laravelコード

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index($id='noname',$pass='unknown') {
+
+        global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee;
+   margin:-40px 0px -50px 0px; }
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+
+function tag($tag, $txt) {
+   return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+
+   public function index() {
+       global $head, $style, $body, $end;
+
+       $html = $head . tag('title','Hello/Index') . $style . $body
+           . tag('h1','Index') . tag('p','this is Index page')
+           . '<a href="/hello/other">go to Other page</a>'
+         . $end;
+       return $html;
+   }
+
+   public function other() {
+       global $head, $style, $body, $end;
+
+       $html = $head . tag('title','Hello/Other') . $style . $body
+           . tag('h1','Other') . tag('p','this is Other page')
+           . $end;
+       return $html;
+   }
+
+}
+
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+Route::get('hello/other','HelloController@other');
